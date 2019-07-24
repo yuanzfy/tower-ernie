3f8064d2d6fed46312faf55482d7419b1ed5d22f
yuanzfy <yuanzfy@163.com>
2019-07-24 14:03:27 +0800

    fix

--- ERNIE/finetune/classifier.py
+++ ERNIE/finetune/classifier.py
@@ -68,7 +68,7 @@ def create_model(args, pyreader_name, ernie_config, is_prediction=False):
     cls_feats_a = ernie_a.get_pooled_output()
     cls_feats_b = ernie_b.get_pooled_output()
     cls_feats = fluid.layers.dropout(
-        x=[cls_feats_a, cls_feats_b],
+        x=fluid.layers.concat([cls_feats_a, cls_feats_b], name="cls_concat"),
         dropout_prob=0.1,
         dropout_implementation="upscale_in_train")
     logits = fluid.layers.fc(
